7e368e541978f20c33fe25cd0b81b8725c57bae2
ruether <jan.ruether@adesso.de>
2017-02-23 18:12:27 +0100

    Anzahl an möglichen Gleisen und Weichen erhöht

--- src/app/schaltplan/schaltplan.component.ts
+++ src/app/schaltplan/schaltplan.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {Baustein, Gleis, Richtung} from "../shared/Schiene";
+import {Baustein, Gleis, Weiche, Richtung} from "../shared/Schiene";
 import {GleisbauerService} from "../gleisbauer/gleisbauer.service";
 
 @Component({
@@ -16,9 +16,22 @@ export class SchaltplanComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.schaltplan = this.gleisbauerService.neu(30, 30).setzePosition(10, 10)
+    this.schaltplan = this.gleisbauerService.neu(20, 20).setzePosition(10, 10)
       .setzeBausteinAnAktuellePosition(new Gleis(Richtung.VERTICAL))
       .setzeBausteinUntenDaneben(new Gleis(Richtung.VON_OBEN_NACH_LINKS_UNTEN))
+      .setzeBausteinLinksUntenDaneben(new Gleis(Richtung.VON_LINKS_NACH_RECHTS_OBEN))
+      .setzeBausteinLinksDaneben(new Weiche(Richtung.VON_LINKS_NACH_RECHTS_UNTEN))
+      .setzeBausteinLinksDaneben(new Weiche(Richtung.VON_RECHTS_NACH_LINKS_OBEN))
+      .setzeBausteinLinksObenDaneben(new Gleis(Richtung.VON_LINKS_NACH_RECHTS_UNTEN))
+      .setzeBausteinLinksDaneben(new Gleis(Richtung.HORIZONTAL))
+      .setzeBausteinLinksDaneben(new Gleis(Richtung.HORIZONTAL))
+      .setzeBausteinLinksDaneben(new Gleis(Richtung.VON_RECHTS_NACH_LINKS_UNTEN))
+      .setzeBausteinLinksUntenDaneben(new Weiche(Richtung.VON_LINKS_UNTEN_NACH_RECHTS_OBEN))
+      .setzeBausteinRechtsDaneben(new Gleis(Richtung.HORIZONTAL))
+      .setzeBausteinRechtsDaneben(new Gleis(Richtung.HORIZONTAL))
+      .setzeBausteinRechtsDaneben(new Gleis(Richtung.HORIZONTAL))
+      .setzeBausteinRechtsDaneben(new Gleis(Richtung.HORIZONTAL))
+
       .end();
   }
 
--- src/app/shared/Schiene.ts
+++ src/app/shared/Schiene.ts
@@ -26,6 +26,26 @@ export class Gleis implements Baustein {
     switch (richtung) {
       case Richtung.HORIZONTAL :
         return gibBildordner() + "gleis_horizontal.png";
+      case Richtung.VERTICAL :
+        return gibBildordner() + "gleis_vertical.png";
+
+      case Richtung.VON_LINKS_NACH_RECHTS_OBEN :
+        return gibBildordner() + "gleis_von_links_nach_rechts_oben.png";
+      case Richtung.VON_LINKS_NACH_RECHTS_UNTEN :
+        return gibBildordner() + "gleis_von_links_nach_rechts_unten.png";
+      case Richtung.VON_OBEN_NACH_LINKS_UNTEN :
+        return gibBildordner() + "gleis_von_oben_nach_links_unten.png";
+      case Richtung.VON_OBEN_NACH_RECHTS_UNTEN :
+        return gibBildordner() + "gleis_von_oben_nach_rechts_unten.png";
+      case Richtung.VON_RECHTS_NACH_LINKS_OBEN :
+        return gibBildordner() + "gleis_von_rechts_nach_links_oben.png";
+      case Richtung.VON_RECHTS_NACH_LINKS_UNTEN :
+        return gibBildordner() + "gleis_von_rechts_nach_links_unten.png";
+      case Richtung.VON_UNTEN_NACH_LINKS_OBEN :
+        return gibBildordner() + "gleis_von_unten_nach_links_oben.png";
+      case Richtung.VON_UNTEN_NACH_RECHTS_OBEN :
+        return gibBildordner() + "gleis_von_unten_nach_rechts_oben.png";
+
       default :
         throw Error();
     }
@@ -43,6 +63,22 @@ export class Weiche implements Gleis {
     switch (richtung) {
       case Richtung.VON_LINKS_NACH_RECHTS_OBEN :
         return gibBildordner() + "weiche_von_links_nach_rechts_oben.png";
+      case Richtung.VON_LINKS_NACH_RECHTS_UNTEN :
+        return gibBildordner() + "weiche_von_links_nach_rechts_unten.png";
+      case Richtung.VON_RECHTS_NACH_LINKS_OBEN :
+        return gibBildordner() + "weiche_von_rechts_nach_links_oben.png";
+      case Richtung.VON_RECHTS_NACH_LINKS_UNTEN:
+        return gibBildordner() + "weiche_von_rechts_nach_links_unten.png";
+
+      case Richtung.VON_LINKS_OBEN_NACH_RECHTS_UNTEN :
+        return gibBildordner() + "weiche_von_links_oben_nach_rechts_unten.png";
+      case Richtung.VON_LINKS_UNTEN_NACH_RECHTS_OBEN :
+        return gibBildordner() + "weiche_von_links_unten_nach_rechts_oben.png";
+      case Richtung.VON_RECHTS_OBEN_NACH_LINKS_UNTEN :
+        return gibBildordner() + "weiche_von_rechts_oben_nach_links_unten.png";
+      case Richtung.VON_RECHTS_UNTEN_NACH_LINKS_OBEN:
+        return gibBildordner() + "weiche_von_rechts_unten_nach_links_oben.png";
+
       default :
         throw Error();
     }
@@ -55,5 +91,16 @@ export enum Richtung {
   VERTICAL,
 
   VON_LINKS_NACH_RECHTS_OBEN,
-  VON_OBEN_NACH_LINKS_UNTEN
+  VON_LINKS_NACH_RECHTS_UNTEN,
+  VON_RECHTS_NACH_LINKS_OBEN,
+  VON_RECHTS_NACH_LINKS_UNTEN,
+  VON_OBEN_NACH_LINKS_UNTEN,
+  VON_OBEN_NACH_RECHTS_UNTEN,
+  VON_UNTEN_NACH_LINKS_OBEN,
+  VON_UNTEN_NACH_RECHTS_OBEN,
+
+  VON_LINKS_UNTEN_NACH_RECHTS_OBEN,
+  VON_LINKS_OBEN_NACH_RECHTS_UNTEN,
+  VON_RECHTS_UNTEN_NACH_LINKS_OBEN,
+  VON_RECHTS_OBEN_NACH_LINKS_UNTEN
 }
