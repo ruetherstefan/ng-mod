5ea426d4df1e382214a12fa38693c186c15b5de2
Jan Rüther <jan.ruether@adesso.de>
2017-08-28 18:26:53 +0200

    Vorbereitung Gleismarkierung poc

---
+++ apps/grafiken/tilesets/spr60-1.tsx
@@ -0,0 +1,61 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Modellbahntileset1" tilewidth="32" tileheight="32" tilecount="19" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="../../../../P/Modellbahn/Dokumente/spdrs60 png/gleis_horizontal.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="../../../../P/Modellbahn/Dokumente/spdrs60 png/gleis_vertical.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="../../../../P/Modellbahn/Dokumente/spdrs60 png/gleis_von_links_nach_rechts_oben.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="../../../../P/Modellbahn/Dokumente/spdrs60 png/gleis_von_links_nach_rechts_unten.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="../../../../P/Modellbahn/Dokumente/spdrs60 png/gleis_von_oben_nach_rechts_unten.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="../../../../P/Modellbahn/Dokumente/spdrs60 png/gleis_von_rechts_nach_links_oben.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="../../../../P/Modellbahn/Dokumente/spdrs60 png/gleis_von_rechts_nach_links_unten.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="../../../../P/Modellbahn/Dokumente/spdrs60 png/gleis_von_unten_nach_links_oben.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="../../../../P/Modellbahn/Dokumente/spdrs60 png/gleis_von_unten_nach_rechts_oben.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="32" source="../../../../P/Modellbahn/Dokumente/spdrs60 png/leer.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="32" source="../../../../P/Modellbahn/Dokumente/spdrs60 png/weiche_von_links_nach_rechts_oben.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="../../../../P/Modellbahn/Dokumente/spdrs60 png/weiche_von_links_nach_rechts_unten.png"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="../../../../P/Modellbahn/Dokumente/spdrs60 png/weiche_von_links_oben_nach_rechts_unten.png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="32" source="../../../../P/Modellbahn/Dokumente/spdrs60 png/weiche_von_links_unten_nach_rechts_oben.png"/>
+ </tile>
+ <tile id="15">
+  <image width="32" height="32" source="../../../../P/Modellbahn/Dokumente/spdrs60 png/weiche_von_rechts_nach_links_oben.png"/>
+ </tile>
+ <tile id="16">
+  <image width="32" height="32" source="../../../../P/Modellbahn/Dokumente/spdrs60 png/weiche_von_rechts_nach_links_unten.png"/>
+ </tile>
+ <tile id="17">
+  <image width="32" height="32" source="../../../../P/Modellbahn/Dokumente/spdrs60 png/weiche_von_rechts_oben_nach_links_unten.png"/>
+ </tile>
+ <tile id="18">
+  <image width="32" height="32" source="../../../../P/Modellbahn/Dokumente/spdrs60 png/weiche_von_rechts_unten_nach_links_oben.png"/>
+ </tile>
+ <tile id="24">
+  <image width="32" height="32" source="../spr60/png/gleis_von_oben_nach_links_unten.png"/>
+ </tile>
+</tileset>
