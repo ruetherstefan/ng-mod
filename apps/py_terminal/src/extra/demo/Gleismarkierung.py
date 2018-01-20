439252e92bde8d778d4c0828734469aff7f5e068
Jan Rüther <jan.ruether@adesso.de>
2018-01-20 12:05:08 +0100

    poc Bildoverlay

---
+++ apps/py_terminal/src/extra/demo/Gleismarkierung.py
@@ -0,0 +1,73 @@
+import pygame
+import os
+
+from src.baustein.Gleis import *
+from src.baustein.Baustein import Gleisschrauber
+
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+_image_library = {}
+def get_image(path):
+        global _image_library
+        image = _image_library.get(path)
+        if image == None:
+                canonicalized_path = path.replace('/', os.sep).replace('\\', os.sep)
+                image = pygame.image.load(canonicalized_path)
+                _image_library[path] = image
+        return image
+
+pygame.init()
+
+
+size = (1000, 800)
+screen = pygame.display.set_mode(size)
+
+pygame.display.set_caption("Gleismarkierung Demo")
+
+
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+
+# Loop until the user clicks the close button.
+done = False
+
+
+# -------- Main Program Loop -----------
+while not done:
+    # --- Main event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        elif event.type == pygame.KEYDOWN:
+            print("User pressed a key.")
+        elif event.type == pygame.KEYUP:
+            print("User let go of a key.")
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            print("User pressed a mouse button")
+
+    # --- Game logic should go here
+
+    # --- Screen-clearing code goes here
+    screen.fill(WHITE)
+
+    # --- Drawing code should go here
+
+    screen.blit(get_image("../../../pixmaps/" + "poc/Bild.png"), [100, 300])
+
+    screen.blit(get_image("../../../pixmaps/" + "poc/Bild.png"), [100, 400])
+    screen.blit(get_image("../../../pixmaps/" + "poc/WeicheRechtsMitte.png"), [100, 400])
+
+
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+
+    # --- Limit to 60 frames per second
+    clock.tick(60)
+
+# Close the window and quit.
+pygame.quit()
+
