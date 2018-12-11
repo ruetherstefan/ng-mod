9d515b356a94d39367be34d5bc3336592309b9c3
Jan Rüther <jan.ruether@adesso.de>
2018-12-11 17:47:33 +0100

    Test Refactored

--- apps/py_terminal/test/baustein/Weiche_test.py
+++ apps/py_terminal/test/baustein/Weiche_test.py
@@ -8,31 +8,31 @@ from src.baustein.Weichenstellung import Weichenstellung
 
 
 @pytest.fixture
-def screen():
+def screen(monkeypatch):
     import pygame
     pygame_screen = Mock(spec=pygame.display)
 
+    monkeypatch.setattr('src.baustein.Weiche.get_image', MagicMock(return_value=""))
 
-@patch('src.baustein.Weiche.get_image')
+
 @patch('src.baustein.Weiche.WeichenControlBote')
-def test_aendere_weichenstellung_weicheadresse_weitergabe(weichen_control_bote_mock, get_image, screen):
+def test_aendere_weichenstellung_weicheadresse_weitergabe(weichen_control_bote_mock, screen):
     weiche = WeicheRechtsNachUnten(screen, Weichenadresse.W1)
     weiche.aendere_weichenstellung()
     weichen_control_bote_mock().aendere_weichenstellung.assert_called_with(Weichenadresse.W1, ANY)
 
 
-@patch('src.baustein.Weiche.get_image')
 @patch('src.baustein.Weiche.WeichenControlBote')
-def test_aendere_weichenstellung_gerade_zu_abzweigend(weichen_control_bote_mock, get_image, screen):
+def test_aendere_weichenstellung_gerade_zu_abzweigend(weichen_control_bote_mock, screen):
     weiche = WeicheRechtsNachUnten(screen, Weichenadresse.W1)
     weiche.weichenstellung = Weichenstellung.GERADE
     weiche.aendere_weichenstellung()
-    weichen_control_bote_mock().aendere_weichenstellung.assert_called_with(Weichenadresse.W1, Weichenstellung.ABZWEIGEND)
+    weichen_control_bote_mock().aendere_weichenstellung.assert_called_with(ANY, Weichenstellung.ABZWEIGEND)
 
-@patch('src.baustein.Weiche.get_image')
+
 @patch('src.baustein.Weiche.WeichenControlBote')
-def test_aendere_weichenstellung_abzweigend_zu_gerade(weichen_control_bote_mock, get_image, screen):
+def test_aendere_weichenstellung_abzweigend_zu_gerade(weichen_control_bote_mock, screen):
     weiche = WeicheRechtsNachUnten(screen, Weichenadresse.W1)
     weiche.weichenstellung = Weichenstellung.ABZWEIGEND
     weiche.aendere_weichenstellung()
-    weichen_control_bote_mock().aendere_weichenstellung.assert_called_with(Weichenadresse.W1, Weichenstellung.GERADE)
+    weichen_control_bote_mock().aendere_weichenstellung.assert_called_with(ANY, Weichenstellung.GERADE)
