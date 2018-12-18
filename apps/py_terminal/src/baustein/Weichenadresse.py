7b36d670dd90ff177983fc21ea2c871cfe44ddf5
Jan Rüther <jan.ruether@adesso.de>
2018-12-18 09:17:44 +0100

    Adressen (Weichennamen) Ebene1

--- apps/py_terminal/src/baustein/Weichenadresse.py
+++ apps/py_terminal/src/baustein/Weichenadresse.py
@@ -12,4 +12,18 @@ class Weichenadresse(Enum):
     W8 = 8
     W9 = 9
     W10 = 10
+
+    #Ebene 1
+    W20 = 20
+    W21 = 21
+    W22 = 22
+    W23 = 23
+    W24 = 24
+    W25 = 25
+    W26 = 26
+    W27 = 27
+    W28 = 28
+    W29 = 29
+    W30 = 30
+
     Undefiniert = 9999
