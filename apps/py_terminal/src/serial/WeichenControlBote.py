95387965f62f2896d540487302045164af075d96
Jan Rüther <jan.ruether@adesso.de>
2019-05-18 12:59:36 +0200

    Weichenadressen schätzend erweitert

--- apps/py_terminal/src/serial/WeichenControlBote.py
+++ apps/py_terminal/src/serial/WeichenControlBote.py
@@ -6,11 +6,34 @@ from src.baustein.Weichenadresse import Weichenadresse
 
 class WeichenControlBote:
 
-    weichenadresse_mapping = {Weichenadresse.W1: b'\01',
-                              Weichenadresse.W2: b'\02'}
+    weichenadresse_mapping = {
+                            Weichenadresse.W1: b'\09',
+                            Weichenadresse.W2: b'\10',
+                            Weichenadresse.W3: b'\11',
+                            Weichenadresse.W4: b'\12',
+                            Weichenadresse.W5: b'\13',
+                            Weichenadresse.W6: b'\14',
+                            Weichenadresse.W7: b'\15',
+                            Weichenadresse.W8: b'\16',
+                            Weichenadresse.W9: b'\19',
+                            Weichenadresse.W10: b'\20',
+
+                            # Ebene 1
+                            Weichenadresse.W20: b'\01',
+                            Weichenadresse.W21: b'\01',
+                            Weichenadresse.W22: b'\02',
+                            Weichenadresse.W23: b'\03',
+                            Weichenadresse.W24: b'\04',
+                            Weichenadresse.W25: b'\05',
+                            Weichenadresse.W26: b'\06',
+                            Weichenadresse.W27: b'\07',
+                            Weichenadresse.W28: b'\08',
+                            Weichenadresse.W29: b'\01',
+                            Weichenadresse.W30: b'\01',
+                            }
 
     def aendere_weichenstellung(self, weichenadresse, weichenstellung):
-        print(weichenadresse, str(Weichenstellung.GERADE == weichenstellung))
+        print(weichenadresse, self.weichenadresse_mapping.get(weichenadresse), str(Weichenstellung.GERADE == weichenstellung))
         weichen_control.turnout_set_for_route(self.weichenadresse_mapping.get(weichenadresse), b'\00',
                                               Weichenstellung.GERADE == weichenstellung)
         weichen_control.turnout_free(self.weichenadresse_mapping.get(weichenadresse), b'\00')
