adf143ec0e2978affbad7cb1d965f18bf1e0cd8d
Jan Rüther <jan.ruether@adesso.de>
2021-01-09 11:38:43 +0100

    Grafiken für Fahrstraßen

--- apps/grafiken/tilesets/spr60Markierungen-1.tsx
+++ apps/grafiken/tilesets/spr60Markierungen-1.tsx
@@ -1,10 +1,46 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="MarkierungenSpr60-1" tilewidth="32" tileheight="32" tilecount="2" columns="0">
+<tileset name="MarkierungenSpr60-1" tilewidth="32" tileheight="32" tilecount="14" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="32" height="32" source="../spr60/png/Markierungen/WeicheRechtsMitte.png"/>
  </tile>
  <tile id="1">
   <image width="32" height="32" source="../spr60/png/Markierungen/WeicheRechtsOben.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="../spr60/png/Markierungen/WeicheLinksMitte.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="../spr60/png/Markierungen/WeicheLinksOben.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="../spr60/png/Markierungen/WeicheLinksUnten.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="../spr60/png/Markierungen/WeicheRechtsUnten.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="../spr60/png/Markierungen/Fahrstrasse/FahrstrasseWeicheLinksMitte.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="../spr60/png/Markierungen/Fahrstrasse/FahrstrasseWeicheLinksOben.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="../spr60/png/Markierungen/Fahrstrasse/FahrstrasseWeicheLinksUnten.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="../spr60/png/Markierungen/Fahrstrasse/FahrstrasseWeicheRechtsMitte.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="32" source="../spr60/png/Markierungen/Fahrstrasse/FahrstrasseWeicheRechtsOben.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="32" source="../spr60/png/Markierungen/Fahrstrasse/FahrstrasseWeicheRechtsUnten.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="../spr60/png/Markierungen/GleisHorizontal.png"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="../spr60/png/Markierungen/GleisVertikal.png"/>
  </tile>
 </tileset>
