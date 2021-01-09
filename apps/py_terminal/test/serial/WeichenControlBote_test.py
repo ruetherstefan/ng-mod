31ee1780004fb9508902fd4a9d570bdaf36c5683
Jan Rüther <jan.ruether@adesso.de>
2021-01-09 13:24:17 +0100

    Test an Anschluss Anlage anpassen

--- apps/py_terminal/test/serial/WeichenControlBote_test.py
+++ apps/py_terminal/test/serial/WeichenControlBote_test.py
@@ -20,9 +20,9 @@ def test_aendere_weichenstellung__ruft_serial_abzweigend(weichen_control):
 @patch('src.serial.WeichenControlBote.weichen_control')
 def test_aendere_weichenstellung__W1_ruft_Adresse1(weichen_control):
     WeichenControlBote().aendere_weichenstellung(Weichenadresse.W1, Weichenstellung.GERADE)
-    weichen_control.turnout_set_for_route.assert_called_with(b'\01', b'\00', ANY)
+    weichen_control.turnout_set_for_route.assert_called_with(b'\12', b'\00', ANY)
 
 @patch('src.serial.WeichenControlBote.weichen_control')
 def test_aendere_weichenstellung__W2_ruft_Adresse2(weichen_control):
     WeichenControlBote().aendere_weichenstellung(Weichenadresse.W2, Weichenstellung.GERADE)
-    weichen_control.turnout_set_for_route.assert_called_with(b'\02', b'\00', ANY)
+    weichen_control.turnout_set_for_route.assert_called_with(b'\14', b'\00', ANY)
