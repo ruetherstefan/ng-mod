29dc15c60d929401fabf6e0cba125950d5610b9f
ruether <jan.ruether@adesso.de>
2021-12-27 11:45:08 +0100

    stellpult / Main Datei aufgeräumt

--- src/main/stellpult.py
+++ src/main/stellpult.py
@@ -1,49 +1,34 @@
-from src.baustein.Gleis import *
+import pygame
+
 from src.baustein.Weiche import Weiche
 from src.main.Streckenplaner import Streckenplaner
 from src.serial.WeichenControlBote import weichen_control
 
-import pygame
-
-
-BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
 
 pygame.init()
 
-#setup serial port
+# setup serial port
 weichen_control.initialisation()
-
 
 size = (1000, 800)
 screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption("Stellpult Prototyp")
 
-
 gleise = Streckenplaner().plane_ennepetal(screen)
-
-
 
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
-# Loop until the user clicks the close button.
-done = False
-
 
 # -------- Main Program Loop -----------
+done = False
 while not done:
     # --- Main event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-        elif event.type == pygame.KEYDOWN:
-            print("User pressed a key.")
-        elif event.type == pygame.KEYUP:
-            print("User let go of a key.")
         elif event.type == pygame.MOUSEBUTTONDOWN:
             for gleis in gleise:
                 if gleis.bild.get_rect().move(gleis.get_position()).collidepoint(event.pos):
@@ -54,7 +39,6 @@ while not done:
                         if isinstance(gleis, Weiche):
                             gleis.toggleFahrstrasse()
 
-
     # --- Game logic should go here
 
     # --- Screen-clearing code goes here
@@ -63,7 +47,6 @@ while not done:
     # --- Drawing code should go here
     for gleis in gleise:
         gleis.draw()
-
 
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
@@ -74,6 +57,5 @@ while not done:
 # Close the window and quit.
 pygame.quit()
 
-#tear down serial port
+# tear down serial port
 weichen_control.de_initialisation()
-
