e4a3c8a5bf2c7acfdc1912062b6fcab91ecb9914
erfinder <jan@hansen-ruether.de>
2022-04-29 19:40:00 +0200

    Vorbereitung serieller Befehl für die Lok. In dem die einzelnen Bytes in Funktionen gebildet werden.

--- src/serial/LokControlBote.py
+++ src/serial/LokControlBote.py
@@ -3,10 +3,38 @@ from src.model.Lok import Lok
 
 class LokControlBote:
     def lok_fahre(self, lok: Lok):
-        pass
-        #ser.write(self.get_low_byte_adresse(lok.adresse))
-        #answer = ser.read()
+        ser.write(self.get_adresse_low_byte(lok.adresse))
+        answer = ser.read()
 
-    def get_low_byte_adresse(self, adresse: int):
+        ser.write(self.get_adresse_high_byte(lok.adresse))
+        answer = ser.read()
+
+        ser.write(lok.speed)
+        answer = ser.read()
+
+        ser.write(self.get_byte_funktionen(lok))
+        answer = ser.read()
+
+    def get_adresse_low_byte(self, adresse: int):
         my_byte = adresse & 0xFF
         return my_byte
+
+    def get_adresse_high_byte(self, adresse: int):
+        my_byte = adresse >> 8
+        return my_byte
+
+    def get_byte_funktionen(self, lok: Lok):
+        byte_funktion = 0xc0       # default bit 6 und 7
+        if lok.forwaerts:
+            byte_funktion += 0x20      # forwaerts bit 5 setzen
+        if lok.frontlicht:
+            byte_funktion += 0x10      # frontlicht bit 4 setzen
+        if lok.f4:
+            byte_funktion += 0x08      # funktion F4 bit 3 setzen
+        if lok.f3:
+            byte_funktion += 0x04      # funktion F3 bit 2 setzen
+        if lok.f2:
+            byte_funktion += 0x02      # funktion F2 bit 1 setzen
+        if lok.f1:
+            byte_funktion += 0x01      # funktion F1 bit 0 setzen
+        return byte_funktion
--- test/serial/LokControlBote_test.py
+++ test/serial/LokControlBote_test.py
@@ -1,5 +1,95 @@
+from src.model.Lok import Lok
 from src.serial.LokControlBote import LokControlBote
 
 
 def test_get_low_byte_adresse_1():
-    assert 1 == LokControlBote().get_low_byte_adresse(1)
+    assert LokControlBote().get_adresse_low_byte(1) == 1
+
+
+def test_get_low_byte_adresse_2():
+    assert LokControlBote().get_adresse_low_byte(2) == 2
+
+
+def test_get_low_byte_adresse_256():
+    assert LokControlBote().get_adresse_low_byte(256) == 0
+
+
+def test_get_high_byte_adresse_1():
+    assert LokControlBote().get_adresse_high_byte(1) == 0
+
+
+def test_get_high_byte_adresse_256():
+    assert LokControlBote().get_adresse_high_byte(256) == 1
+
+
+def test_get_byte_funktionen_default_nur6und7gesetzt():
+    assert LokControlBote().get_byte_funktionen(Lok(1)) & 0xC0 == 0xC0
+
+
+def test_get_byte_funktionen_forwaerts_5gesetzt():
+    lok = Lok(1)
+    lok.forwaerts = True
+    assert LokControlBote().get_byte_funktionen(lok) & 0x20 == 0x20
+
+def test_get_byte_funktionen_rueckwaerts_5nichtgesetzt():
+    lok = Lok(1)
+    lok.forwaerts = False
+    assert LokControlBote().get_byte_funktionen(lok) & 0x20 == 0
+
+
+
+def test_get_byte_funktionen_frontlicht_4gesetzt():
+    lok = Lok(1)
+    lok.frontlicht = True
+    assert LokControlBote().get_byte_funktionen(lok) & 0x10 == 0x10
+
+def test_get_byte_funktionen_frontlicht_4nichtgesetzt():
+    lok = Lok(1)
+    lok.frontlicht = False
+    assert LokControlBote().get_byte_funktionen(lok) & 0x10 == 0
+
+
+
+def test_get_byte_funktionen_F4_3gesetzt():
+    lok = Lok(1)
+    lok.f4 = True
+    assert LokControlBote().get_byte_funktionen(lok) & 0x08 == 0x08
+
+def test_get_byte_funktionen_F4_3nichtgesetzt():
+    lok = Lok(1)
+    lok.f4 = False
+    assert LokControlBote().get_byte_funktionen(lok) & 0x08 == 0
+
+
+
+def test_get_byte_funktionen_F3_2gesetzt():
+    lok = Lok(1)
+    lok.f3 = True
+    assert LokControlBote().get_byte_funktionen(lok) & 0x04 == 0x04
+
+def test_get_byte_funktionen_F3_2nichtgesetzt():
+    lok = Lok(1)
+    lok.f3 = False
+    assert LokControlBote().get_byte_funktionen(lok) & 0x04 == 0
+
+
+def test_get_byte_funktionen_F2_1gesetzt():
+    lok = Lok(1)
+    lok.f2 = True
+    assert LokControlBote().get_byte_funktionen(lok) & 0x02 == 0x02
+
+def test_get_byte_funktionen_F2_1nichtgesetzt():
+    lok = Lok(1)
+    lok.f2 = False
+    assert LokControlBote().get_byte_funktionen(lok) & 0x02 == 0
+
+
+def test_get_byte_funktionen_F1_0gesetzt():
+    lok = Lok(1)
+    lok.f1 = True
+    assert LokControlBote().get_byte_funktionen(lok) & 0x01 == 0x01
+
+def test_get_byte_funktionen_F1_0nichtgesetzt():
+    lok = Lok(1)
+    lok.f1 = False
+    assert LokControlBote().get_byte_funktionen(lok) & 0x01 == 0
