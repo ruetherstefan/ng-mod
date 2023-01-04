adf2107a263382e2c1c135208aa9a7c12d29796e
ruether <jan.ruether@adesso.de>
2023-01-04 12:47:04 +0100

    Signal88ControlBote - Erfolgreiches Mapping - Changed Flag

--- src/serial/Signal88ControlBote.py
+++ src/serial/Signal88ControlBote.py
@@ -1,15 +1,37 @@
 from unittest.mock import Mock
 
 from src.model.BesetztModul import BesetztModul
+from src.model.BesetztModulAdresse import BesetztModulAdresse
 from src.serial import SerialConnector
 from src.serial.Signal88Control import Signal88Control
 
 
 class Signal88ControlBote:
     def __init__(self):
+        self.besetzt_modul_adress_mappings__module1: {BesetztModulAdresse: int} = {}
+
         self.signal_88_control = Signal88Control()
         if SerialConnector.is_offline():
             self.signal_88_control = Mock(spec=Signal88Control)
 
     def update_module(self, besetzt_module: [BesetztModul]):
-        return self.signal_88_control.lese_signale(1)
+        besetzt_module_map: {BesetztModulAdresse: BesetztModul} = self.erstelle_besetzt_modul_map_zur_adresse(besetzt_module)
+        aenderungs_flag = False
+
+        modul1 = self.signal_88_control.lese_signale(1)
+        for adresse in self.besetzt_modul_adress_mappings__module1:
+
+            ausgelesener_wert = modul1[self.besetzt_modul_adress_mappings__module1[adresse]]
+            if besetzt_module_map[adresse].besetzt != ausgelesener_wert:
+                besetzt_module_map[adresse].besetzt = ausgelesener_wert
+                aenderungs_flag = True
+
+        return aenderungs_flag
+
+    @staticmethod
+    def erstelle_besetzt_modul_map_zur_adresse(besetzt_module: [BesetztModul]):
+        besetzt_module_map: {BesetztModulAdresse: BesetztModul} = {}
+        for modul in besetzt_module:
+            besetzt_module_map.update({modul.adresse: modul})
+        return besetzt_module_map
+
--- test/serial/Signal88ControlBote_test.py
+++ test/serial/Signal88ControlBote_test.py
@@ -9,9 +9,68 @@ def test_mock_wenn_offline():
     assert isinstance(Signal88ControlBote().signal_88_control, Mock)
 
 
-def test_update_module__keine_aenderung():
+def gegeben_bote_mit_control_returning(control_return):
     bote: Signal88ControlBote = Signal88ControlBote()
-    bote.signal_88_control.lese_signale = Mock(return_value=[False, False])
+    bote.signal_88_control.lese_signale = Mock(return_value=control_return)
+    bote.besetzt_modul_adress_mappings__module1 = {BesetztModulAdresse.H1: 0,
+                                                   BesetztModulAdresse.H2: 1}
+    return bote
 
-    besetzt_module: [BesetztModul] = [BesetztModul(BesetztModulAdresse.H1)]
-    assert [False, False] == bote.update_module(besetzt_module)
+
+def gegeben2modul_mit_besetzt_status(besetzt1, besetzt2):
+    modul1: BesetztModul = BesetztModul(BesetztModulAdresse.H1)
+    modul1.besetzt = besetzt1
+
+    modul2: BesetztModul = BesetztModul(BesetztModulAdresse.H2)
+    modul2.besetzt = besetzt2
+    return modul1, modul2
+
+
+def test_update_module__keine_aenderung_false():
+    bote = gegeben_bote_mit_control_returning([False, False, False, False, False, False, False, False])
+    modul1, modul2 = gegeben2modul_mit_besetzt_status(False, False)
+
+    bote.update_module([modul1, modul2])
+    assert not modul1.besetzt
+    assert not modul2.besetzt
+
+
+def test_update_module__keine_aenderung_true():
+    bote = gegeben_bote_mit_control_returning([True, False, False, False, False, False, False, False])
+    modul1, modul2 = gegeben2modul_mit_besetzt_status(True, False)
+
+    bote.update_module([modul1, modul2])
+    assert modul1.besetzt
+    assert not modul2.besetzt
+
+
+def test_update_module__aenderung_true_mapping1():
+    bote = gegeben_bote_mit_control_returning([True, False, False, False, False, False, False, False])
+    modul1, modul2 = gegeben2modul_mit_besetzt_status(False, False)
+
+    bote.update_module([modul1, modul2])
+    assert modul1.besetzt
+    assert not modul2.besetzt
+
+
+def test_update_module__aenderung_true_mapping2():
+    bote = gegeben_bote_mit_control_returning([False, True, False, False, False, False, False, False])
+    modul1, modul2 = gegeben2modul_mit_besetzt_status(False, False)
+
+    bote.update_module([modul1, modul2])
+    assert not modul1.besetzt
+    assert modul2.besetzt
+
+
+def test_update_module__aenderung_wird_angezeigt_false():
+    bote = gegeben_bote_mit_control_returning([True, False, False, False, False, False, False, False])
+    modul1, modul2 = gegeben2modul_mit_besetzt_status(True, False)
+
+    assert not bote.update_module([modul1, modul2])
+
+
+def test_update_module__aenderung_wird_angezeigt_true():
+    bote = gegeben_bote_mit_control_returning([True, False, False, False, False, False, False, False])
+    modul1, modul2 = gegeben2modul_mit_besetzt_status(False, False)
+
+    assert bote.update_module([modul1, modul2])
