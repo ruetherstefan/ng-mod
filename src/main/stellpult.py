f270b5c3816b1dba90262cd6a17100b100c96580
ruether <jan.ruether@adesso.de>
2023-02-07 17:08:53 +0100

    Platzhalter Code für den Test der ersten automatischen Fahrt in Ennepetal

--- src/main/stellpult.py
+++ src/main/stellpult.py
@@ -1,7 +1,13 @@
 import pygame
 
 from src.controller.Streckenplaner import Streckenplaner
+from src.controller.ZugController import ZugController
+from src.model.BesetztModul import BesetztModul
+from src.model.BesetztModulAdresse import BesetztModulAdresse
+from src.model.zug.Fahrstrecke import Fahrstrecke
+from src.model.zug.Zug import Zug
 from src.serial import SerialConnector
+from src.serial.Signal88ControlBote import Signal88ControlBote
 from src.view.Streckenmaler import Streckenmaler
 
 WHITE = (255, 255, 255)
@@ -19,6 +25,20 @@ pygame.display.set_caption("Stellpult Prototyp")
 
 ennepetal_model = Streckenplaner().plane_ennepetal_model()
 ennepetal_view = Streckenmaler(ennepetal_model).plane_ennepetal_view(screen)
+
+# Besetztmodule init
+besetzt_module: [BesetztModul] = [BesetztModul(BesetztModulAdresse.H1),
+                                  BesetztModul(BesetztModulAdresse.H2),
+                                  BesetztModul(BesetztModulAdresse.H3)]
+
+# Test Züge init
+zug: Zug = Zug()
+zug.ende = besetzt_module[0]
+zug.anfang = besetzt_module[0]
+
+# Test Fahrstrecke definition
+fahrstrecken: [Fahrstrecke] = []
+#TODO init fahrstrecke hin
 
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
@@ -38,6 +58,24 @@ while not done:
                     baustein_view.click(event)
 
     # --- Game logic should go here
+
+    # Besetztabfrage
+    # TODO Signal88ControlBote besetzt_modul_adress_mappings__module1 schreiben
+    Signal88ControlBote().update_module(besetzt_module)
+
+    for fahrstrecke in fahrstrecken:
+        ZugController().update_zug_position(zug, fahrstrecke)
+        ZugController.update_zug_speed(zug, fahrstrecke)
+
+    # falls in Anfang oder Zielposition
+    #if zug.anfang is besetzt_module[0]:
+        #setzte hin
+    #if zug.anfang is besetzt_module[-1]:
+        # setzte zurück
+
+    # Route löschen, falls existiert
+    # neue Route stellen, hin oder zurück
+
 
     # --- Screen-clearing code goes here
     screen.fill(WHITE)
