8d304e1f6e84348bab5e9b6fdeb392485e16c203
ruether <jan.ruether@adesso.de>
2023-10-09 12:44:09 +0200

    Mapping vorbereitet

--- src/serial/Signal88ControlBote.py
+++ src/serial/Signal88ControlBote.py
@@ -9,9 +9,19 @@ from src.serial.Signal88Control import Signal88Control
 class Signal88ControlBote:
     besetzt_modul_adress_mappings__module1: {BesetztModulAdresse: int} = {
         BesetztModulAdresse.B001_HAUPT_AUSFAHRT_LINKS: 0,
-        BesetztModulAdresse.B011_HAUPT_G1_HALTE_LINKS: 1,
-        BesetztModulAdresse.B012_HAUPT_G1_MITTE: 2,
-        BesetztModulAdresse.B013_HAUPT_G1_HALTE_RECHTS: 3}
+        BesetztModulAdresse.B002_HAUPT_AUSFAHRT_RECHTS_G1_SCHATTEN: 1,
+        BesetztModulAdresse.B003_HAUPT_AUSFAHRT_RECHTS_G2_UND_G3_GUETER: 2,
+        BesetztModulAdresse.B011_HAUPT_G1_HALTE_LINKS: 3,
+        BesetztModulAdresse.B012_HAUPT_G1_MITTE: 4,
+        BesetztModulAdresse.B013_HAUPT_G1_HALTE_RECHTS: 5,
+        BesetztModulAdresse.B021_HAUPT_G2_HALTE_LINKS: 6,
+        BesetztModulAdresse.B022_HAUPT_G2_MITTE: 7,
+        BesetztModulAdresse.B023_HAUPT_G2_HALTE_RECHTS: 8,
+        BesetztModulAdresse.B031_HAUPT_G3_HALTE_LINKS: 9,
+        BesetztModulAdresse.B032_HAUPT_G3_MITTE: 10,
+        BesetztModulAdresse.B033_HAUPT_G3_HALTE_RECHTS: 11
+    }
+
     fake_results = {BesetztModulAdresse.B001_HAUPT_AUSFAHRT_LINKS: True,
                     BesetztModulAdresse.B011_HAUPT_G1_HALTE_LINKS: False,
                     BesetztModulAdresse.B012_HAUPT_G1_MITTE: False,
