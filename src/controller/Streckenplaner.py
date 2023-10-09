a985d928053c3411854fccd976524d60f6d538ab
ruether <jan.ruether@adesso.de>
2023-10-09 12:39:17 +0200

    Weichenbesetztmeldungen hbf fertig

--- src/controller/Streckenplaner.py
+++ src/controller/Streckenplaner.py
@@ -11,6 +11,10 @@ class Streckenplaner:
         for adresse in Weichenadresse:
             if adresse in [Weichenadresse.W24, Weichenadresse.W25]:
                 besetzt_modul = verwalter.get(BesetztModulAdresse.B001_HAUPT_AUSFAHRT_LINKS)
+            elif adresse in [Weichenadresse.W21]:
+                besetzt_modul = verwalter.get(BesetztModulAdresse.B002_HAUPT_AUSFAHRT_RECHTS_G1_SCHATTEN)
+            elif adresse in [Weichenadresse.W20, Weichenadresse.W22, Weichenadresse.W23]:
+                besetzt_modul = verwalter.get(BesetztModulAdresse.B003_HAUPT_AUSFAHRT_RECHTS_G2_UND_G3_GUETER)
             else:
                 besetzt_modul = BesetztModul(BesetztModulAdresse.NOCH_NICHT_BESTIMMT)
 
